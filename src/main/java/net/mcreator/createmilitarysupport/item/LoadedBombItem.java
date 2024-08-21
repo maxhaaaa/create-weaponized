cbcc4b6bda8eb3705b535d20574474494cb694e8
p51mustangp47 <kinse@192.168.254.14>
2024-08-21 13:32:02 -0700

    added creative tab to bombtestingblock

--- src/main/java/net/mcreator/createmilitarysupport/item/LoadedBombItem.java
+++ src/main/java/net/mcreator/createmilitarysupport/item/LoadedBombItem.java
@@ -1,7 +1,18 @@
 
 package net.mcreator.createmilitarysupport.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+
+import net.mcreator.createmilitarysupport.procedures.LoadedBombRightclickedProcedure;
 
 public class LoadedBombItem extends Item {
 	public LoadedBombItem() {
@@ -17,7 +28,7 @@ public class LoadedBombItem extends Item {
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
 		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
-		LoadedBombRightclickedProcedure.execute();
+		LoadedBombRightclickedProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ(), ar.getObject());
 		return ar;
 	}
 }
